02d8636b0d0c7265650e8fb81d9589a64456f37e
extropyst <77421034+extropyst@users.noreply.github.com>
2026-09-25 23:59:45 -0300

    refactor: remover codigo de Flutter en rama main y conservar solo Fase 1 y documentos

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:agecare_app/core/config/app_config.dart';
-
-void main() {
-  test('AppConfig expone el nombre de la app', () {
-    expect(AppConfig.appName, 'AgeCare');
-  });
-}
